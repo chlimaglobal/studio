c545c985fc54277a28658c0b42cbbffff70a17b7
Marcos Ch Lima <CHLIMAGLOBAL@gmail.com>
2025-07-22 01:20:03 +0000

    Erro

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -17,6 +18,19 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'publickey-credentials-create=*',
+          },
+        ],
+      },
+    ];
   },
 };
 
