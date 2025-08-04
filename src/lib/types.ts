4ec0e81b01895c2e6a0ef04f112e2add2d38e50f
Marcos Ch Lima <CHLIMAGLOBAL@gmail.com>
2025-08-04 22:48:41 +0000

    é pra deixar salvar qualquer valor de saída, até R$10.000,00

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -35,7 +35,7 @@ export const TransactionFormSchema = z.object({
   description: z.string().min(2, {
     message: "A descrição deve ter pelo menos 2 caracteres.",
   }),
-  amount: z.coerce.number({invalid_type_error: "Por favor, insira um valor válido."}).positive({ message: "O valor deve ser um número positivo." }),
+  amount: z.coerce.number({invalid_type_error: "Por favor, insira um valor válido."}).min(0.01, { message: "O valor deve ser de pelo menos R$0,01." }),
   date: z.date({required_error: "Por favor, selecione uma data."}),
   type: z.enum(['income', 'expense']),
   category: z.enum(transactionCategories as [string, ...string[]], {
