9e2b3a3a5e005de20e06662becb2ee7dc11f884a
Marcos Ch Lima <CHLIMAGLOBAL@gmail.com>
2025-08-11 23:18:22 +0000

    Resolva a parte de notificação. Deise pronta para aparecer notificação p

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,9 +1,7 @@
+// This file must be in the public folder.
 
-// This file needs to be in the public directory.
-
-// Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,25 +14,20 @@ const firebaseConfig = {
   "messagingSenderId": "123511329863"
 };
 
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
 const messaging = firebase.messaging();
 
-// Handle incoming messages. Called when:
-// - received while app is in browser tab (foreground)
-// - received when app is in background
-// - received when app is closed
 messaging.onBackgroundMessage((payload) => {
   console.log(
-    '[firebase-messaging-sw.js] Received background message ',
+    "[firebase-messaging-sw.js] Received background message ",
     payload
   );
   
-  // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.icon || '/icon.png'
+    icon: payload.notification.icon || '/icon.png',
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
