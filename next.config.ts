49c62de8f8657b2b06abf18e42d2d0a1f17b45e1
Marcos Ch Lima <CHLIMAGLOBAL@gmail.com>
2025-08-22 21:00:38 +0000

    Agora coloque para quando o usuário adicionar o app no atalho da tela do

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,41 @@ const withPWA = require('next-pwa')({
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
   publicExcludes: ['!*'], // Garante que nenhum arquivo da pasta public seja excluído
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+    dynamicStartUrl: true,
+    customWorkerDir: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    sw: "sw.js",
+    scope: "/",
+    cacheStartUrl: true,
+    fallbacks: {
+      image: "/icon-192x192.png",
+    },
+    workboxOptions: {
+      maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB
+    },
+    // Adicione esta parte para definir os ícones
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any maskable"
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable"
+      }
+    ]
+  },
 });
 
 const nextConfig: NextConfig = {
