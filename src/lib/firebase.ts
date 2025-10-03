22a5de05b532b1f95f22b86aefbbf682ad708296
Marcos Ch Lima <CHLIMAGLOBAL@gmail.com>
2025-10-03 03:45:01 +0000

    Ao configurar restrições de aplicativos na sua chave, apenas um tipo de

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -8,10 +9,10 @@ import { getMessaging, isSupported } from "firebase/messaging";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  "apiKey": "b2baa9ff-ac6f-4413-80a9-eacd43c20c04",
   "projectId": "financeflow-we0in",
   "appId": "1:123511329863:web:a81c91b72098fa668d8d62",
   "storageBucket": "financeflow-we0in.firebasestorage.app",
-  "apiKey": "AIzaSyC5d98JbKWbtkXyFKQui2baPdVmdgRbzas",
   "authDomain": "financeflow-we0in.firebaseapp.com",
   "measurementId": "G-EW74L3HEX7",
   "messagingSenderId": "123511329863"
