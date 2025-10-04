25f91cf87e2fa54c81c2356eae26e45b82507454
Marcos Ch Lima <CHLIMAGLOBAL@gmail.com>
2025-10-04 01:07:38 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,13 +9,13 @@ import { getMessaging, isSupported } from "firebase/messaging";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  "apiKey": "b2baa9ff-ac6f-4413-80a9-eacd43c20c04",
-  "projectId": "financeflow-we0in",
-  "appId": "1:123511329863:web:a81c91b72098fa668d8d62",
-  "storageBucket": "financeflow-we0in.firebasestorage.app",
+  "apiKey": "AIzaSyC8i_841JmY1TfA-Gk9pXyY2s-k3g0u8Yw",
   "authDomain": "financeflow-we0in.firebaseapp.com",
-  "measurementId": "G-EW74L3HEX7",
-  "messagingSenderId": "123511329863"
+  "projectId": "financeflow-we0in",
+  "storageBucket": "financeflow-we0in.appspot.com",
+  "messagingSenderId": "233857398324",
+  "appId": "1:233857398324:web:e0f8b704c7c89b37803ba9",
+  "measurementId": "G-5511W442E5"
 };
 
 // Initialize Firebase
