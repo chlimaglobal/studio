d6b0b30d8df6f4959aa0908efe96e0dd0246bc6a
Marcos Ch Lima <CHLIMAGLOBAL@gmail.com>
2025-11-05 18:56:19 +0000

    Try fixing this error: `Runtime Error: Error: An unexpected response was

--- src/next.config.ts
+++ src/next.config.ts
@@ -39,11 +39,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
-  },
 };
 
 // Conditionally apply PWA wrapper. It's not compatible with Turbopack.
