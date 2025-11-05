e9dd41bea0bb429f70efd0066f88baeb6e9a6410
Marcos Ch Lima <CHLIMAGLOBAL@gmail.com>
2025-11-05 21:50:52 +0000

    Ainda continua semanrir a visualização

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,31 @@
-// @ts-check
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-}
+  devIndicators: {
+    allowedDevOrigins: [
+      "*.cluster-qhrn7lb3szcfcud6uanedbkjnm.cloudworkstations.dev"
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: false,
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+       {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+export default nextConfig;
