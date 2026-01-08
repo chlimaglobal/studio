d0caf7ae74bbaa973ff55055b02f5614506469b8
Marcos Ch Lima <CHLIMAGLOBAL@gmail.com>
2026-01-08 02:53:57 +0000

    Porque tá insistindo na versão 14.2.4? Eu já enviei duas vezes que é pra

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // Adicione outras configurações do Next.js aqui, se necessário
-};
-
-export default nextConfig;
