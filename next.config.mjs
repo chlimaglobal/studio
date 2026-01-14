e240d925d7b0d69efa41d09fc952efae249aceaf
Marcos Ch Lima <CHLIMAGLOBAL@gmail.com>
2026-01-14 23:25:35 +0000

    Perfeito — agora ficou claro e objetivo. O erro não tem relação com Fire

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,16 @@
-/** @type {import('next').NextConfig} */
-import PWA from '@ducanh2912/next-pwa';
 
-const withPWA = PWA({
+import nextPWA from '@ducanh2912/next-pwa';
+
+const withPWA = nextPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
 });
 
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default withPWA(nextConfig);
