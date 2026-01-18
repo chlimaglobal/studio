a6cded82ec031ee585017dbccd1cd7227fe1a00a
Marcos Ch Lima <CHLIMAGLOBAL@gmail.com>
2026-01-18 00:12:03 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,12 +8,12 @@ import { getFunctions } from 'firebase/functions';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyD7H3wU8pB8pH-xmTxJhUZCsPa-PP_L0cY",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "financeflow-we0in.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "financeflow-we0in",
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "123511329863",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:123511329863:web:a81c91b72098fa668d8d62",
 };
 
 // Validação para garantir que as configurações essenciais não estão vazias
